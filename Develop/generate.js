668a47d0bf49a82650124b863590bbf658ad7c2a
HawaFallay <113000340+HawaFallay@users.noreply.github.com>
2023-02-23 14:33:00 -0800

    added button

--- Develop/generate.js
+++ Develop/generate.js
@@ -1,18 +1,22 @@
 
 // moment
-const moment = document.querySelector("moment");
-console.log (moment);
+const time = moment().format("MMMM DD YYYY");
+console.log (time);
+$("#currentDay").text(time)
+//let yearChosen = new Date().getFullYear(); // 2023
+// let monthChosen = new Date().getMonth(); // 11
 
-let yearChosen = new Date().getFullYear(); // 2023
-let monthChosen = new Date().getMonth(); // 11
+// function getNumberOfDays(year, month) {
+// console.log(year, month);
+// let numDays = new Date (year, month + 1, 0).getDate();
+// return numDays;
+// }//
 
-function getNumberOfDays(year, month) {
-console.log(year, month);
-let numDays = new Date (year, month + 1, 0).getDate();
-return numDays;
-}
-
-console.log(getNumberOfDays)(2023,1)();
 
 var saveButton = $(".saveBtn");
-console.log (saveBtn);
+console.log (saveButton);
+saveButton.on("click",function(){
+    var idValue=$(this).parent().attr("id")
+    var texValue=$(this).siblings(".description").val()
+    localStorage.setItem(idValue, texValue)
+})
