37ef90e2608145128668393bac428042a701534d
HawaFallay <113000340+HawaFallay@users.noreply.github.com>
2023-02-23 14:40:46 -0800

    used local storage getItem

--- Develop/generate.js
+++ Develop/generate.js
@@ -20,3 +20,13 @@ saveButton.on("click",function(){
     var texValue=$(this).siblings(".description").val()
     localStorage.setItem(idValue, texValue)
 })
+
+$("#9 .description").val(localStorage.getItem("9"))
+$("#10 .description").val(localStorage.getItem("10"))
+$("#11 .description").val(localStorage.getItem("11"))
+$("#12 .description").val(localStorage.getItem("12"))
+$("#13 .description").val(localStorage.getItem("13"))
+$("#14 .description").val(localStorage.getItem("14"))
+$("#15 .description").val(localStorage.getItem("15"))
+$("#16 .description").val(localStorage.getItem("16"))
+$("#17 .description").val(localStorage.getItem("17"))
